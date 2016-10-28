21a911d49ac7b5b3aad8eeca461de7cbf500da5c
Frank Harvey <frankharvey88@gmail.com>
2016-10-28 15:10:16 +0000

    Completed solution

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var months = ['January','February','March','April','May','June',
 app.get('*', function(req, res){
     var dateString = req.url.slice(1).replace(/%20/g,' ')
     if (dateString.length === 0){
-        var msg = "Enter a date into the address bar as such:\n"
+        var msg = "Enter a date into the address bar as such:\n\n"
         msg += "https://fcc-api-projects-superking84.c9users.io/October 20, 2016\n"
         msg += "https://fcc-api-projects-superking84.c9users.io/1476921600000"
         res.end(msg)
@@ -17,22 +17,21 @@ app.get('*', function(req, res){
         natural: null
     }
     
-    var isValidDate
+    var date
     if (isNaN(dateString)){
-        var date = new Date(dateString)
-        isValidDate = !isNaN(Date.parse(date))
-        if (!isValidDate){
+        date = new Date(dateString)
+        
+        if (isNaN(Date.parse(date))){
             res.end(JSON.stringify(output))
         }
         
-        output['natural'] = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
         output['unix'] = Date.parse(dateString) / 1000
     } else {
-        var date = new Date(parseInt(dateString) * 1000)
+        date = new Date(parseInt(dateString) * 1000)
         
-        output['unix'] = dateString
-        output['natural'] = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+        output['unix'] = parseInt(dateString)
     }
+    output['natural'] = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     
     res.end(JSON.stringify(output))
 })
